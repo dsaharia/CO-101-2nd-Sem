2335ce25ce28e5eb5668872990fa48a34c147a3c
Dhiraj Saharia <dhirajspeaks@gmail.com>
2018-05-10 13:15:40 +0530

    Create q8.c

---
+++ Class Test 2/q8.c
@@ -0,0 +1,8 @@
+/*8. Run a system command of Operating system and store the result of the command in a file (Use argc, *argv[] and any other necessary operations). (marks: 2)
+*/
+#include<stdio.h>
+int main()
+{
+    system("touch dhiraj.txt"); // To create a file name dhiraj.txt
+    return 0;
+}
