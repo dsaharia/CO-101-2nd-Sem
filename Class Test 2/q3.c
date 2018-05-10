f30a09a4a124d11786c39983897ee47e5e6fb6dc
Dhiraj Saharia <dhirajspeaks@gmail.com>
2018-05-10 12:22:50 +0530

    Create q3.c

---
+++ Class Test 2/q3.c
@@ -0,0 +1,21 @@
+/*3. Allocate space for an array using calloc(). (marks: 1)
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n=15,i,*p;
+    p=(int*)calloc(n, sizeof(int));  
+    if(p == NULL)                     
+    {
+        printf("memory cannot be allocated\n");
+    }
+    else{
+      printf("Elements of array are\n");
+      for(i=0;i<n;i++)
+      {
+        printf("%d\n",*(p+i));
+      }
+    }
+    return 0;
+}
